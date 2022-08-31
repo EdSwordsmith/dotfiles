c63b22a8fd5610086571be1d24c2dd61395be45d
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2022-09-01 00:25:43 +0100

    bat

--- home.nix
+++ home.nix
@@ -45,6 +45,8 @@
     enableAliases = true;
   };
 
+  programs.bat.enable = true;
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards

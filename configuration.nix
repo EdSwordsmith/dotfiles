2db6bbbce4d06a2d2d49fecd9f2e14e8ff8595ea
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2022-08-31 20:15:35 +0100

    im dumb

--- configuration.nix
+++ configuration.nix
@@ -101,7 +101,7 @@
     wget
   ];
 
-  environment.variables = { EDITOR = "vim"; };
+  environment.variables = { EDITOR = "nvim"; };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.

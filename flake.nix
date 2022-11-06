d67d325d17e85c0fcdb6fc6186802eda780efcb4
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2022-11-06 13:44:57 +0000

    Stable channel pls :(

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";

07da4f3a247fac2d09e1bffe922a7f1040620cdb
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2022-11-15 18:34:25 +0000

    Programs module for real

---
+++ modules/programs.nix
@@ -0,0 +1,33 @@
+{ config, options, pkgs, lib, ... }:
+
+{
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+    wget
+    docker-compose
+    htop
+    ripgrep
+    fd
+  ];
+
+  hm.home.packages = with pkgs; [
+    spotify
+    brave
+    firefox
+    discord
+    vscode
+    emacs28NativeComp
+    jetbrains.idea-ultimate
+    jetbrains-mono
+  ];
+
+  hm.programs.exa = {
+    enable = true;
+    enableAliases = true;
+  };
+
+  hm.programs.bat.enable = true;
+}

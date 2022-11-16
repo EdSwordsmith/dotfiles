54bf19b5ee124b2047e8a38914217c8b5ec95f04
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2022-11-16 17:58:41 +0000

    dumb dumb

---
+++ modules/djtobis.nix
@@ -0,0 +1,20 @@
+{ config, options, pkgs, lib, inputs, configDir, ... }:
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.modules.pombobot;
+  inherit (inputs) jmusicbot;
+in
+{
+  options.modules.djtobis.enable = mkEnableOption "DJ Tóbis";
+
+  config = mkIf cfg.enable {
+    systemd.services.djtobis = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.jre8}/bin/java -Dnogui=true -jar ${jmusicbot}";
+        WorkingDirectory = "${configDir}/djtobis";
+      };
+    };
+  };
+}

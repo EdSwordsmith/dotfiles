05fef2902b74a66a64918c51150f0d48ed735316
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-01-16 16:49:34 +0000

    remove virtualbox

--- hosts/fornost/configuration.nix
+++ hosts/fornost/configuration.nix
@@ -19,20 +19,6 @@
     git.enable = true;
   };
 
-  # Virtualbox
-  virtualisation.virtualbox = {
-    host = {
-      enable = true;
-      enableExtensionPack = true;
-    };
-
-    #guest = {
-    #  enable = true;
-    #  x11 = true;
-    #};
-  };
-  users.extraGroups.vboxusers.members = [ "eduardo" ];
-
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Bootloader.

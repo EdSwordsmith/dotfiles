cbe7c796d632d2e8677bbbf155025502f6d47988
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-03-01 10:02:25 +0000

    chore: add bash scripts from the NixOS wiki to the sway module

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -2,6 +2,47 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.edu.sway;
+
+  # bash script to let dbus know about important env variables and
+  # propagate them to relevent services run at the end of sway config
+  # see
+  # https://github.com/emersion/xdg-desktop-portal-wlr/wiki/"It-doesn't-work"-Troubleshooting-Checklist
+  # note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but also restarts  
+  # some user services to make sure they have the correct environment variables
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+
+    text = ''
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+    '';
+  };
+
+
+  # currently, there is some friction between sway and gtk:
+  # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
+  # the suggested way to set gtk settings is with gsettings
+  # for gsettings to work, we need to tell it where the schemas are
+  # using the XDG_DATA_DIR environment variable
+  # run at the end of sway config
+  configure-gtk = pkgs.writeTextFile {
+    name = "configure-gtk";
+    destination = "/bin/configure-gtk";
+    executable = true;
+    text =
+      let
+        schema = pkgs.gsettings-desktop-schemas;
+        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+      in
+      ''
+        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+        gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+      '';
+  };
+
   lockCommand = pkgs.writeShellScriptBin "swaylock" ''
     ${pkgs.swaylock-effects}/bin/swaylock -f \
       --screenshots \
@@ -35,12 +76,16 @@ in
       enable = true;
       wrapperFeatures.gtk = true;
       extraPackages = with pkgs; [
+        dbus-sway-environment
+        configure-gtk
+        lockCommand
+
         swaybg
         swayidle
         foot
         wdisplays
         wl-clipboard
-      ] ++ [ lockCommand ];
+      ];
     };
 
     security.polkit.enable = true;
@@ -216,6 +261,11 @@ in
         for_window [app_id="^launcher$"] floating enable, sticky enable, resize set 30 ppt 60 ppt, border pixel 6
         for_window [app_id="^brave-(?!browser).*"] shortcuts_inhibitor disable
         exec_always ${pkgs.autotiling}/bin/autotiling
+      '';
+
+      extraConfig = ''
+        exec dbus-sway-environment
+        exec configure-gtk
       '';
     };
 

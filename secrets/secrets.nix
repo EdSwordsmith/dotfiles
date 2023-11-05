af7522755b3b770821ce3328d51edd7c2f5aff2d
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-11-05 22:39:33 +0000

    add ssh keys for dolamroth

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,13 @@
 let
   user_annuminas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDr6cqDPQKapijMfCxwXAFSniL5Tl1WMMcJ1dUcB3yhy";
   user_minastirith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHdqPxNoMvoAdSQMug5H2aMnXXQgSpEyh96dibVtxRqd";
-  user_dolamroth = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0/hgrBsENtRhS2KxIsZrXiW5vVlkwxGCCQ14TUtGVd";
+  user_dolamroth = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINXBgSSmDUpSOQb8+trWtlClCwV25h0jBSCwFjm5w2bF";
   user_fornost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9fsZ6NiBTcHQlT7GvX0gjMXkVB1FA4d0ryckaTIod2";
   users = [user_annuminas user_minastirith user_dolamroth user_fornost];
 
   system_minastirith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqqMgK+dTe40QHWrUfRZgBNtBMYUU/4KBt9Lk4iT3aY";
-  systems = [system_minastirith];
+  system_dolamroth = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC46he9/EHBXFfVJDLE7K6i++V4xZrjjPK7v7XVn1kM6";
+  systems = [system_minastirith system_dolamroth];
 in {
   "pombobot.age".publicKeys = users ++ systems;
   "djtobis.age".publicKeys = users ++ systems;

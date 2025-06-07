bf33e384ad43a0edc30c3e3b75f2e167a3f4cc9e
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2025-06-07 18:18:24 +0100

    update nextcloud

--- hosts/dolamroth/configuration.nix
+++ hosts/dolamroth/configuration.nix
@@ -74,10 +74,13 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud30;
+    package = pkgs.nextcloud31;
     hostName = "cloud.espadeiro.pt";
     https = true;
-    config.adminpassFile = config.age.secrets.ncdbpass.path;
+    config = {
+      adminpassFile = config.age.secrets.ncdbpass.path;
+      dbtype = "sqlite";
+    };
   };
 
   services.nginx.enable = true;

8a60155c949cf106211f7cf3cc0944f6420b0af6
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2026-07-18 00:05:30 +0100

    add minas anor

---
+++ hosts/minasanor/configuration.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, profiles, ... }:
+{
+  imports = with profiles; [
+    shell.git.common
+    shell.fish
+    editors.emacs
+    graphical.niri
+    graphical.games
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Lisbon";
+
+  # Configure keymap
+  console.keyMap = "pt-latin1";
+  services.xserver.xkb = {
+    layout = "pt";
+    variant = "";
+  };
+
+  usr.shell = pkgs.fish;
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "26.05"; # Did you read the comment?
+
+}
